13e0b31cc83c69338add62f360be7a3ab0871e89
saritagore21@gmail.com <rahulgore34@gmail.com>
2026-04-04 20:03:08 +0530

    api for azure fn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
@@ -7,9 +8,26 @@ app.use(cors());
 
 app.get('/api/hello', (req, res) => {
     res.json({
-        message: "Hello Rahul from Azure Node API",
+        message: "Hello Rahul from LOCAL Node API",
         timestamp: new Date()
     });
+});
+
+app.get('/api/callazurefn', async (req, res) => {
+      try {
+        const response = await axios.get(
+            'https://expensetrackerfunctionapp-f5fwfygva2h5drcd.centralindia-01.azurewebsites.net/api/httpTrigger1'
+        );
+
+        res.json({
+            source: "Node API",
+            functionResponse: response.data.message
+        });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Error calling Azure Function" });
+    }
 });
 
 const port = process.env.PORT || 3000;
@@ -17,3 +35,5 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+//https://expensetrackernodeapi-c0f6aqbqf6fncpb7.eastasia-01.azurewebsites.net/api/hello
